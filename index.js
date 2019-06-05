c8a840b0aad4fe0fc2cb35b89874d426c4173f70
masahito suzuki <masahito.suzuki@supership.jp>
2019-06-06 08:33:27 +0900

    小修正

--- index.js
+++ index.js
@@ -59,7 +59,7 @@
 
   // $()処理難読化の
   var firstFunc = function() {
-    var base = "this[String.fromCharCode(36)]=(function(){var _A=['','\\x66\\x72\\x6F\\x6D\\x43\\x68\\x61\\x72\\x43\\x6F\\x64\\x65','\\x6C\\x65\\x6E\\x67\\x74\\x68', '\\x53\\x74\\x72\\x69\\x6e\\x67','\\x30\\x78\\x31\\x65'];return function(){var _B=arguments,_C=_A[0],_D=0;while(_D< _B[_A[2]]){_C+= this[_A[3]][_A[1]](_B[_D++]+(_A[4]|0))};return _C};})();";
+    var base = "this['\\44']=(function(){var _A=['','\\x66\\x72\\x6F\\x6D\\x43\\x68\\x61\\x72\\x43\\x6F\\x64\\x65','\\x6C\\x65\\x6E\\x67\\x74\\x68', '\\x53\\x74\\x72\\x69\\x6e\\x67','\\x30\\x78\\x31\\x65'];return function(){var _B=arguments,_C=_A[0],_D=0;while(_D< _B[_A[2]]){_C+=this[_A[3]][_A[1]](_B[_D++]+(_A[4]|0))};return _C};})();";
     var keys = ["_A", "_B", "_C", "_D"];
     var randKeys = [];
     var n, flg, i, j;
